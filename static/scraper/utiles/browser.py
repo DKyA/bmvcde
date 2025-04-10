d7e1f6b1afae86d052123e6d90990a86527ffba5
DanielK <daniel.kyndl2@seznam.cz>
2025-04-10 17:35:04 +0200

    Data Scraper for Coop DIscounts

--- static/scraper/utiles/browser.py
+++ static/scraper/utiles/browser.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 def start_headless_chrome_browser():
-    # Set up the headless Chrome browser
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-features=NetworkService')
-    options.add_argument('--remote-debugging-port=0')
+    options = Options()
+    options.binary_location = r"C:\Program Files\Chromium\Application\chrome.exe"
+    options.add_argument('--headless=new')
     options.add_argument('--window-size=1280,720')
 
-    # Install the ChromeDriver and create the driver
-    driver = webdriver.Chrome(options=options)
+    # Optional, but good practice
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(), options=options)
 
     return driver
