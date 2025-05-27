618fd732f6c8ce9681c2365a1beeafc6e7aa62ee
DanielK <daniel.kyndl2@seznam.cz>
2025-05-27 11:37:13 +0200

    Changing to static adapter CF III

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,15 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			config: undefined, // Use default Cloudflare adapter configuration
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		}),
 	},
 	preprocess: [
