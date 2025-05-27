067c394a7c36e4c00b13a992a1a84e176ee9032a
DanielK <daniel.kyndl2@seznam.cz>
2025-05-27 21:33:17 +0200

    Finished mobile design on main page

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -13,16 +13,25 @@
 				<img src="{base}/images/Logo_main.png" alt="GroceryRunner Logo" width="128" height="96"/>
 			</div>
 		</div>
-		<div class="footer__col">
-			<nav class="footer__nav">
-				<div class="footer__headline">
-					<Underscore> Where <i>Next?</i> </Underscore>
-				</div>
-				<a href="#" class="footer__link">Home</a>
-				<a href="#" class="footer__link">Shopping Basket</a>
-				<a href="#" class="footer__link">Map</a>
-				<a href="#" class="footer__link">Settings</a>
-			</nav>
+		<div class="footer__right">
+			<div class="footer__col">
+				<nav class="footer__nav">
+					<div class="footer__headline">
+						<Underscore> Where <i>Next?</i> </Underscore>
+					</div>
+					<a href="#" class="footer__link">Home</a>
+					<a href="#" class="footer__link">Shopping Basket</a>
+					<a href="#" class="footer__link">Map</a>
+					<a href="#" class="footer__link">Settings</a>
+				</nav>
+			</div>
+			<div class="footer__col footer__col--socials">
+				<a href="#" class="footer__soclink"><i class="bi bi-facebook"></i></a>
+				<a href="#" class="footer__soclink"><i class="bi bi-instagram"></i></a>
+				<a href="#" class="footer__soclink"><i class="bi bi-snapchat"></i></a>
+				<a href="#" class="footer__soclink"><i class="bi bi-bluesky"></i></a>
+				<div class="footer__bg"></div>
+			</div>
 		</div>
 	</div>
 
@@ -33,6 +42,7 @@
 	.footer {
 
 		color: color("text");
+		position: relative;
 
 		&__info {
 			display: flex;
@@ -44,6 +54,43 @@
 
 		}
 
+		&__right {
+			display: flex;
+			justify-content: center;
+			text-align: center;
+			padding: 16px;
+			gap: 16px;
+		}
+
+		&__col {
+			&--socials {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				height: 100%;
+				position: relative;
+				padding: 0 8px 0 16px;
+			}
+		}
+
+		&__bg {
+
+			position: absolute;
+			top: -26px;
+			left: 0;
+			bottom: -26px;
+			width: calc(100% + 48px);
+			background-color: color("primary");
+		}
+
+		&__soclink {
+
+		font-size: 1.2em;
+		color: white;
+		z-index: 2;
+
+		}
 
 		&__headline {
 
